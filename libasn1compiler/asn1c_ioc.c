64788af99e631b9774e962c2b74a14996daa8222
pengrui2009 <pengrui_2009@163.com>
2021-05-25 10:49:35 +0800

    Feature: 1.support 32/64 bit system. 2.remove some warning info, when generate codes.

--- libasn1compiler/asn1c_ioc.c
+++ libasn1compiler/asn1c_ioc.c
@@ -282,7 +282,7 @@ emit_ioc_table(arg_t *arg, asn1p_expr_t *context, asn1c_ioc_table_and_objset_t i
     OUT("static const asn_ioc_set_t asn_IOS_%s_%d[] = {\n",
         MKID(ioc_tao.objset), ioc_tao.objset->_type_unique_index);
     INDENT(+1);
-    OUT("%zu, %zu, asn_IOS_%s_%d_rows\n", ioc_tao.ioct->rows, columns,
+    OUT("{%zu, %zu, asn_IOS_%s_%d_rows}\n", ioc_tao.ioct->rows, columns,
         MKID(ioc_tao.objset), ioc_tao.objset->_type_unique_index);
     INDENT(-1);
     OUT("};\n");
